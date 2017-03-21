df911c66505501741ffd0de16b967b65dcc880c9
Yongjie Liang <jack.liang@gov.ab.ca>
2017-03-21 09:53:31 -0600

    Change deployment flag to c._ckanapi. This is set when _ckanapi in dict in load of ckanapi site package.

--- ckanext/ab_scheming/logic/action/create.py
+++ ckanext/ab_scheming/logic/action/create.py
@@ -2,15 +2,15 @@ import pylons.config as config
 import ckan.plugins.toolkit as toolkit
 from ckan.logic.action import create
 from . import change_pkg_dict_for_import_deployment
-
+from ckan.plugins.toolkit  import c
 
 def package_create(context, data_dict):
-    deployment_mode = toolkit.asbool(config.get('ckan.ab_scheming.deployment', False))
-    # need to change data_dict if import from ckanapi
-    if deployment_mode:
+    if '_ckanapi' in data_dict: # ckanapi call
         data_dict = change_pkg_dict_for_import_deployment(data_dict, 'create')
         if data_dict['type'] != 'documentation':
             context['defer_commit'] = True 
+        c._ckanapi = '_ckanapi'
+        del data_dict['_ckanapi']
     return create.package_create(context, data_dict)
 
 
--- ckanext/ab_scheming/logic/action/update.py
+++ ckanext/ab_scheming/logic/action/update.py
@@ -2,13 +2,13 @@ import pylons.config as config
 import ckan.plugins.toolkit as toolkit
 from ckan.logic.action import update
 from . import change_pkg_dict_for_import_deployment
-
+from ckan.plugins.toolkit  import c
 
 def package_update(context, data_dict):
-    deployment_mode = toolkit.asbool(config.get('ckan.ab_scheming.deployment', False))
-    # need to change data_dict if import from ckanapi
-    if deployment_mode:
+    if '_ckanapi' in data_dict: # ckanapi call 
         data_dict = change_pkg_dict_for_import_deployment(data_dict, 'update')
         if data_dict['type'] != 'documentation':
-            context['defer_commit'] = True 
+            context['defer_commit'] = True
+        c._ckanapi = '_ckanapi'
+        del data_dict['_ckanapi'] 
     return update.package_update(context, data_dict)
