b5ca9159772a17509c1c74145a84f50f9b0bd3a1
Yongjie Liang <jack.liang@gov.ab.ca>
2017-03-31 13:18:59 -0600

    Merge from load_to_OGP

--- ckanext/ab_scheming/logic/action/create.py
+++ ckanext/ab_scheming/logic/action/create.py
@@ -2,12 +2,12 @@ import pylons.config as config
 import ckan.plugins.toolkit as toolkit
 from ckan.logic.action import create
 from . import change_pkg_dict_for_import_deployment
-from ckan.plugins.toolkit  import c
+
 
 def package_create(context, data_dict):
     if '_ckanapi' in data_dict: # ckanapi call
         data_dict = change_pkg_dict_for_import_deployment(data_dict, 'create')
-        if data_dict['type'] != 'documentation':
+        if data_dict['type'] in ['publications', 'opendata']:
             context['defer_commit'] = True 
         c._ckanapi = '_ckanapi'
         del data_dict['_ckanapi']
